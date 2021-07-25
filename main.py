9e45377468de2828bf98bbabc773e6528e6628f3
Bardin Petr <bardin.petr@gmail.com>
2021-07-25 11:57:29 +0300

    Minor fixes

--- main.py
+++ main.py
@@ -1,106 +1,86 @@
-import os
 import threading
-
-from PIL import Image
-from src.sim.MainDevices.ClassicChannel import ClassicChannel
+from os import getcwd
+from time import sleep
 
 from src.connections.Bridge import Bridge
-from src.crypto.Crypto import Crypto
 from src.crypto.KeyManager import KeyManager
+from src.math.QBERGen import key_gen, key_with_mist_gen
 from src.sim.data.HardwareParams import HardwareParams
 from src.sim.devices.OpticFiber import OpticFiber
 from src.sim.devices.users.Alice import Alice
 from src.sim.devices.users.Bob import Bob
 from src.sim.devices.users.EndpointDevice import EndpointDevice
-from src.statistics.StatisticsWriter import StatisticsWriter
-
-i = 1
+from src.statistics.StatisticsAggregator import StatisticsAggregator
 
 
-# TODO: replace classic channel with Bridge
 def main():
+    km0 = KeyManager(directory=f'{getcwd()}/data/alice')
+    b0 = Bridge(
+        '127.0.0.1', '10.10.10.1', '255.255.255.0',
+        data_port=58001, wave_port=58002,
+        dlm_ports=(58003, 58004),
+        user_mode=Bridge.USER_ALICE
+    )
+    b0.subscribe(Bridge.EVENT_INCOMING_CRYPT, lambda x: print(x))
+    # b0.subscribe(Bridge.EVENT_INCOMING_WAVES, lambda x: print("W", x))
+
+    km1 = KeyManager(directory=f'{getcwd()}/data/bob', is_bob=True)
+    b1 = Bridge(
+        '127.0.0.2', '10.10.10.2', '255.255.255.0',
+        data_port=59001, wave_port=59002,
+        dlm_ports=(58003, 58004),
+        user_mode=Bridge.USER_BOB
+    )
+    b1.subscribe(Bridge.EVENT_INCOMING_CRYPT, lambda x: print(x))
+
+    b0.connect(b1.ext_ip, km0, 59001, 59002)
+    b1.register_connection(b0.ext_ip, km1)
+
+    b0.run()
+    b1.run()
+
+    # Optics
     hp = HardwareParams(
         polarization=(1, 0),
-        # laser_period=5000,
-        # mu=1,
-        # delta_opt=0,
-        # prob_opt=0,
-        # pdc=10 ** -5,
-        # eff=0.1,
-        # dt=1000,
-        # fiber_length=50
+        attenuation=0,
+        delta_opt=0
     )
 
-    sw = StatisticsWriter(f'{os.getcwd()}/data/statistics.json')
+    stat = StatisticsAggregator(hp)
+    stat.subscribe(StatisticsAggregator.EVENT_RESULT, stat.log_statistics)
 
-    km_alice = KeyManager(directory=f'{os.getcwd()}/data/alice')
-    alice_c = Crypto(km_alice)
-    alice_b = Bridge('0.0.0.0', '10.10.10.1', in_port=50001)
-    alice_b.register_crypto('127.0.0.1', alice_c)
-    threading.Thread(target=alice_b.run, daemon=True).run()
-
-    km_bob = KeyManager(directory=f'{os.getcwd()}/data/bob')
-    bob_c = Crypto(km_bob)
-    bob_b = Bridge('127.0.0.1', '10.10.10.2', in_port=50002)
-    bob_b.register_crypto('0.0.0.0', bob_c)
-    threading.Thread(target=bob_b.run, daemon=True).run()
-
-    bob_b.connect('0.0.0.0', 50001)
-
-    cc = ClassicChannel(ClassicChannel.MODE_LOCAL)
-
-    stat = Statistics(hp)
-    stat.subscribe(Statistics.EVENT_RESULT, sw.write)
-    stat.subscribe(Statistics.EVENT_RESULT, stat.log_statistics)
-
-    alice = Alice(hp, classic_channel=cc, session_size=10 ** 4)
+    alice = Alice(hp, session_size=10 ** 2)
     alice.subscribe(EndpointDevice.EVENT_KEY_FINISHED, stat.alice_update)
-    alice.subscribe(EndpointDevice.EVENT_KEY_FINISHED, lambda data: km_alice.append(data[0]))
+    alice.subscribe(EndpointDevice.EVENT_KEY_FINISHED, lambda data: km0.append(data[0]))
+    alice.bind_bridge(b1.ext_ip, b0)
 
     of = OpticFiber(length=hp.fiber_length, deltaopt=hp.delta_opt, probopt=hp.prob_opt)
     alice.forward_link(of)
 
-    bob = Bob(hp, classic_channel=cc)
+    bob = Bob(hp)
     bob.subscribe(EndpointDevice.EVENT_KEY_FINISHED, stat.bob_update)
-    bob.subscribe(EndpointDevicdde.EVENT_KEY_FINISHED, lambda data: km_bob.append(data[0]))
+    bob.subscribe(EndpointDevice.EVENT_KEY_FINISHED, lambda data: km1.append(data[0]))
+    bob.bind_bridge(b0.ext_ip, b1)
 
     of.forward_link(bob)
+    threading.Thread(target=lambda: alice.start(progress_bar=True), daemon=True).run()
 
-    def send():
-        i = 14
-        im = Image.open(f"cat{i}.jpg")
-        # im = im.crop(())
-        im = im.resize((300, 300))
-        im.show()
-        k = im.tobytes()  # open('poem.txt', 'rb').read()
-        print(len(k) * 8)
+    sleep(1)
 
-        def recv(x):
-            global i
-            print(len(x))
-            im = Image.frombytes('RGB', (300, 300), x)
-            im.save(f'res{i}.jpg')
-            im.show()
-            i += 1
-            km_alice.cur_pos = 0
-            km_bob.cur_pos = 0
-            km_alice.save_cur_pos()
-            km_bob.save_cur_pos()
-            # send()
-            # with open('res_poem.txt', 'wb') as f:
-            # f.write(x)
-            # print(np.sum(BinaryFile('poem.txt').read_all() != BinaryFile('res_poem.txt').read_all()) / (len(x) * 8))
+    length = 10 ** 4 + 8
+    qber = 0.05
 
-        alice_b.subscribe(Bridge.EVENT_SOCKET_INCOMING, recv)
+    key_without_errors = key_gen(length)
+    key_with_errors = key_with_mist_gen(key_without_errors, qber)
 
-        bob_b.send_crypt('0.0.0.0', k)
+    km0.append(key_without_errors)
+    km1.append(key_with_errors)
 
-    # threading.Thread(target=send, daemon=True).run()
-    threading.Thread(target=lambda: alice.start(progress_bar=False), daemon=True).run()
+    print('successful!')
 
     while True:
-        pass
+        sleep(1)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
